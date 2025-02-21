49124c8859413eeba67d0a49eebd3128d0450a03
ido8450 <ido8450@gmail.com>
2025-02-21 14:06:43 +0200

    fix connection to mongo

--- back/src/constants/congif.ts
+++ back/src/constants/congif.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const mongoURI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/local";
 
 export const getToken = () => {
   if (!process.env.TOKEN_SECRET) {
