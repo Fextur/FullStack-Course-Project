be8ad6403165658801439891774fbbae3548c4ee
ido8450 <ido8450@gmail.com>
2025-03-02 11:42:02 +0200

    added cookie parser lib and using cors better now in back

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import { mongoURI, PORT } from "./constants/config";
+import { CLIENT_URL, mongoURI, PORT } from "./constants/config";
 import userRoutes from "./routes/userRoute";
 import postRoutes from "./routes/postRoute";
 import commentRoutes from "./routes/commentRoute";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(mongoURI)
