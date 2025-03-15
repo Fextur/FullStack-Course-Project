d1720963ed5d70bc6034141e3a454f7573047024
ido8450 <ido8450@gmail.com>
2025-03-15 19:25:57 +0200

    added tests

---
+++ back/src/tests/comments.test.ts
@@ -0,0 +1,91 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import postModel from "../models/postModel";
+import userModel from "../models/userModel";
+import commentModel from "../models/commentModel"; // Add the comment model
+import { mongoURI } from "../constants/config";
+import { server } from "../index";
+
+interface IUser {
+  email: string;
+  password: string;
+  username: string;
+  _id?: string;
+  refreshToken?: string[];
+}
+
+type User = IUser & { token?: string };
+const testUser: User = {
+  email: "test@user.com",
+  username: "username",
+  password: "testpassword",
+};
+
+let postId = "";
+
+beforeAll(async () => {
+  await mongoose.connect(mongoURI);
+
+  await postModel.deleteMany();
+  await userModel.deleteMany();
+  await commentModel.deleteMany();
+
+  await request(server).post("/api/users/").send(testUser);
+  const res = await request(server).post("/api/users/login").send(testUser);
+
+  testUser.token = res.body.accessToken;
+  testUser._id = res.body.user.id;
+  expect(testUser.token).toBeDefined();
+
+  const postResponse = await request(server)
+    .post("/api/posts")
+    .set("Authorization", `Bearer ${testUser.token}`)
+    .send({
+      content: "Test Post Content",
+      image: "Test image",
+    });
+  expect(postResponse.statusCode).toBe(201);
+  postId = postResponse.body.id;
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  server.close();
+});
+
+describe("Comment Tests", () => {
+  test("Test Create Comment", async () => {
+    const response = await request(server)
+      .post("/api/comments")
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        content: "This is a comment on the post",
+        postId,
+      });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.content).toBe("This is a comment on the post");
+    expect(response.body.user.id).toBe(testUser._id);
+  });
+
+  test("Test Get Comments by Post", async () => {
+    const response = await request(server)
+      .get(`/api/comments/${postId}`)
+      .set("Authorization", `Bearer ${testUser.token}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.body[0].content).toBe("This is a comment on the post");
+  });
+
+  test("Test Create Comment Fail (No Content)", async () => {
+    const response = await request(server)
+      .post("/api/comments")
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        postId,
+      });
+
+    expect(response.statusCode).toBe(500);
+  });
+});
---
+++ back/src/tests/posts.test.ts
@@ -0,0 +1,164 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import postModel from "../models/postModel";
+import userModel from "../models/userModel";
+import { mongoURI } from "../constants/config";
+import { server } from "../index";
+
+interface IUser {
+  email: string;
+  password: string;
+  username: string;
+  _id?: string;
+  refreshToken?: string[];
+}
+
+type User = IUser & { token?: string };
+
+const testUser: User = {
+  email: "test@user.com",
+  username: "username",
+  password: "testpassword",
+};
+
+let postId = "";
+
+beforeAll(async () => {
+  await mongoose.connect(mongoURI);
+
+  await postModel.deleteMany();
+  await userModel.deleteMany();
+
+  await request(server).post("/api/users/").send(testUser);
+  const res = await request(server).post("/api/users/login").send(testUser);
+
+  testUser.token = res.body.accessToken;
+  testUser._id = res.body.user.id;
+  expect(testUser.token).toBeDefined();
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  server.close();
+});
+
+describe("Posts Tests", () => {
+  test("Posts test get all", async () => {
+    const response = await request(server)
+      .get("/api/posts")
+      .set("Authorization", `Bearer ${testUser.token}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(0);
+  });
+
+  test("Test Create Post", async () => {
+    const response = await request(server)
+      .post("/api/posts")
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        content: "Test Content",
+        image: "Test image", // change to file later
+      });
+    expect(response.statusCode).toBe(201);
+    expect(response.body.content).toBe("Test Content");
+    expect(response.body.image).toBe("Test image"); // change to file later
+    postId = response.body.id;
+  });
+
+  test("Test get post by owner", async () => {
+    const response = await request(server)
+      .get(`/api/posts/${testUser._id}`)
+      .set("Authorization", `Bearer ${testUser.token}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].content).toBe("Test Content");
+    expect(response.body[0].image).toBe("Test image");
+  });
+
+  test("Test Create Post 2", async () => {
+    const response = await request(server)
+      .post("/api/posts")
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        content: "Test Content 2",
+        image: "Test image 2", // change to file later
+      });
+    expect(response.statusCode).toBe(201);
+  });
+
+  test("Posts test get all 2", async () => {
+    const response = await request(server)
+      .get("/api/posts")
+      .set("Authorization", `Bearer ${testUser.token}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(2);
+  });
+
+  test("Test Delete Post", async () => {
+    const response = await request(server)
+      .delete(`/api/posts/${postId}`)
+      .set("Authorization", `Bearer ${testUser.token}`);
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("Test Create Post fail", async () => {
+    const response = await request(server)
+      .post("/api/posts")
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        content: "Test Content 2",
+      });
+    expect(response.statusCode).toBe(500);
+  });
+
+  test("Test Update Post", async () => {
+    const createResponse = await request(server)
+      .post("/api/posts")
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        content: "Initial Content",
+        image: "Initial Image",
+      });
+    expect(createResponse.statusCode).toBe(201);
+    const postIdToUpdate = createResponse.body.id;
+
+    const updateResponse = await request(server)
+      .put(`/api/posts/${postIdToUpdate}`)
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        content: "Updated Content",
+        image: "Updated Image",
+      });
+
+    expect(updateResponse.statusCode).toBe(200);
+    expect(updateResponse.body.content).toBe("Updated Content");
+    expect(updateResponse.body.image).toBe("Updated Image");
+  });
+
+  test("Test Like Post", async () => {
+    const createResponse = await request(server)
+      .post("/api/posts")
+      .set("Authorization", `Bearer ${testUser.token}`)
+      .send({
+        content: "Content to Like",
+        image: "Image for Like",
+      });
+    expect(createResponse.statusCode).toBe(201);
+    const postIdToLike = createResponse.body.id;
+
+    const likeResponse = await request(server)
+      .post(`/api/posts/like/${postIdToLike}`)
+      .set("Authorization", `Bearer ${testUser.token}`);
+
+    expect(likeResponse.statusCode).toBe(200);
+    expect(likeResponse.body.isUserLiked).toBe(true);
+
+    const unlikeResponse = await request(server)
+      .post(`/api/posts/like/${postIdToLike}`)
+      .set("Authorization", `Bearer ${testUser.token}`);
+
+    expect(unlikeResponse.statusCode).toBe(200);
+    expect(unlikeResponse.body.isUserLiked).toBe(false);
+  });
+});
