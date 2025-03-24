943798fd43ace307c1aeb63d33ded8716ab74c28
ido8450 <ido8450@gmail.com>
2025-03-24 20:38:02 +0200

    Update index.ts

--- back/src/index.ts
+++ back/src/index.ts
@@ -98,11 +98,11 @@ const options = {
 const specs = swaggerJsDoc(options);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
+app.use(express.static("front"));
+
 app.get("*", (_req, res) => {
   res.sendFile(path.resolve(__dirname, "../front", "index.html"));
 });
-
-app.use(express.static("front"));
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
