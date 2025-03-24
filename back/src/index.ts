429ea578c63783b880bf1ee9817a2b38730e06f9
ido8450 <ido8450@gmail.com>
2025-03-24 13:56:02 +0200

    resolved

--- back/src/index.ts
+++ back/src/index.ts
@@ -96,7 +96,7 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(express.static("front"));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
